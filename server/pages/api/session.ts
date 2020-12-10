e2cd18baa0a9d5e344b23b927f602b59d0fb8591
Josh Pollock <jpollock412@gmail.com>
2020-12-10 12:24:53 -0500

    decode repo in session endpoint

--- server/pages/api/session.ts
+++ server/pages/api/session.ts
@@ -1,7 +1,15 @@
+import { decryptSession } from '../../services/UserService'
 import { NextApiRequest, NextApiResponse } from 'next'
 import factory from '../../services/serviceFactories'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	let { session } = await factory(req)
-	res.json({ session })
+	let repo = {}
+	if (session) {
+		let _session = decryptSession(session)
+		if (_session && _session.repo) {
+			repo = _session.repo
+		}
+	}
+	res.json({ session, repo })
 }
