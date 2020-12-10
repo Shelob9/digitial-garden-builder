f059c60408fcd70582af34d98f9b2694f9075826
Josh Pollock <jpollock412@gmail.com>
2020-12-10 11:23:00 -0500

    use octokit core

--- server/pages/api/login/after.js
+++ server/pages/api/login/after.js
@@ -1,25 +1,17 @@
 import { getOctokit, getAuth } from '../../../auth';
+import { Octokit } from "@octokit/core";
+
 let auth = getAuth();
+
+
 export default async (req, res) => {
     const { code } = req.query;
     try {
         const oauthAuthentication = await auth({ type: "oauth", code });
         const { token } = oauthAuthentication;
-        let octokit = getOctokit(token);
-        let repos = await octokit.apps.listInstallationsForAuthenticatedUser();
-        let { data } = repos;
-        let installIds = [];
-        let installs = []
-        if (data.installations) {
-            installIds = data.installations.map(d => d.id);
-            installs = await Promise.all(installIds.map(installation_id => {
-                return  
-                octokit.apps.getInstallation({
-                    installation_id,
-                }).then(r => r.data);
-            }))
-        }
-        res.json({ token, installIds,installs });
+        const octokit = new Octokit({ auth: token });
+        let repos = await octokit.request('GET /installation/repositories')
+        res.json({ token, repos });
     } catch (error) {
         res.status(400).json({ error: { name: error.name, status: error.status } });
     }
