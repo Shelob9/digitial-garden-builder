4a29efbd3e8fc524540fca92a262122c9386aaba
Josh Pollock <jpollock412@gmail.com>
2020-12-10 12:32:30 -0500

    return user with session

--- server/pages/api/session.ts
+++ server/pages/api/session.ts
@@ -4,11 +4,15 @@ import getSession from '../../lib/getSession'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	let session = getSession(req)
 	let repo = {}
+	let user = {
+		name: '',
+	}
 	if (session) {
 		let _session = decryptSession(session)
 		if (_session && _session.repo) {
 			repo = _session.repo
 		}
+		user.name = session.name
 	}
-	res.json({ session, repo })
+	res.json({ session, repo, user })
 }
