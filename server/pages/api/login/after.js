64c7f35307970ff484e45a5a64e0836b58d95763
Josh Pollock <jpollock412@gmail.com>
2020-12-10 11:14:35 -0500

    return installls

--- server/pages/api/login/after.js
+++ server/pages/api/login/after.js
@@ -7,7 +7,19 @@ export default async (req, res) => {
         const { token } = oauthAuthentication;
         let octokit = getOctokit(token);
         let repos = await octokit.apps.listInstallationsForAuthenticatedUser();
-        res.json({ token, repos });
+        let { data } = repos;
+        let installIds = [];
+        let installs = []
+        if (data.installations) {
+            installIds = data.installations.map(d => d.id);
+            installs = await Promise.all(installIds.map(installation_id => {
+                return  
+                octokit.apps.getInstallation({
+                    installation_id,
+                }).then(r => r.data);
+            }))
+        }
+        res.json({ token, installIds,installs });
     } catch (error) {
         res.status(400).json({ error: { name: error.name, status: error.status } });
     }
