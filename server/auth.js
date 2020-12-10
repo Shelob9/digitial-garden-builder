d6e8f2b339ef2c7fdd4244ab24d3f3e07c199f4d
Josh Pollock <jpollock412@gmail.com>
2020-12-10 10:58:33 -0500

    use env var for private key

--- server/auth.js
+++ server/auth.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const fs = require('fs');
 const clientId = process.env.GITHUB_ID;
 const clientSecret = process.env.GITHUB_SECRET;
-const privateKey = fs.readFileSync("./private-key.pem").toString();
+const privateKey = process.env.GITHUB_PRIVATE || fs.readFileSync("./private-key.pem").toString();
 const { createAppAuth } = require("@octokit/auth-app");
 const { Octokit } = require('@octokit/rest');
 
