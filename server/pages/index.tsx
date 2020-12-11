821eb1223804531fd26c67afcce33fa5bcdb4035
Josh Pollock <jpollock412@gmail.com>
2020-12-11 14:56:32 -0500

    message on front

--- server/pages/index.tsx
+++ server/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Index({redirect}) {
         Digital Garden Builder Server
          
       </h1>
-      {redirect && <a href={redirect}>Complete Login</a>}
+      {redirect ? <a href={redirect}>Complete Login</a>: <p>If you tried to login and end up here unclear what to do next, sorry. The URL to redirect back should be in state query param, it's not though...</p>}
     </>
     
   )
