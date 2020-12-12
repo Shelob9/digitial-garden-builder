5b31236f8c756a5489ae2f2e68ed10666efce4f7
Josh Pollock <jpollock412@gmail.com>
2020-12-12 15:18:33 -0500

    Use layout for the login pages

--- client/pages/login/after.tsx
+++ client/pages/login/after.tsx
@@ -1,3 +1,5 @@
+import Layout from "components/Layout";
+import Link from "next/link";
 import useUserToken from "../../hooks/useUserCookie";
 export async function getServerSideProps({  query }) {
 	const { token } = query;
@@ -10,5 +12,19 @@ export async function getServerSideProps({  query }) {
 }
 export default function After(props) {
 	const { token } = useUserToken({ token: props.token });
-	return (<div>Logged In with token {token}</div>)
+	return (
+		<Layout pageDisplayTitle={'Login'}>
+			<section>
+				{token ? <div>Logged in with token {token}</div> : <div>No token, that is bad.</div>}
+			</section>
+			<section>
+				<ul>
+					<li><Link href={'/'}><a>Notes</a></Link></li>
+					<li><Link href={'/settings'}><a>Settings</a></Link></li>
+					<li><a href={'https://docs.digitalgardenbuilder.app'}>Documentation</a></li>
+				</ul>
+			</section>
+			
+		</Layout>
+	)
 };
--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import Layout from "components/Layout";
 import useIsLoggedAuthorized from "hooks/useIsLoggedAuthorized";
 import { useEffect } from "react";
 const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
@@ -6,8 +7,18 @@ const state = encodeURIComponent(process.env.NEXT_PUBLIC_CLIENT_LOGIN_REDIRECT)
 const loginLink = `https://github.com/login/oauth/authorize?client_id=${clientId}&state=${state}`;
 export default function Login() {
     let { isLoggedIn } = useIsLoggedAuthorized();
-    return (<div>
-        <p>{isLoggedIn ? 'Logged In' : 'Not Logged In'}</p>
-        <a href={loginLink}>Login With Github</a>
-    </div>)
+    return (
+        <Layout pageDisplayTitle={'Login'}>
+            <div>
+                <p>{isLoggedIn ? 'Logged In' : 'Not Logged In'}</p>
+                <a href={loginLink}>Login With Github</a>
+                <p>Login to edit your notes or suggest changes.</p>
+            </div>
+            <div>
+                <p>
+                    Right now, this isn't going to work if you're not Josh.  
+                </p>
+            </div>
+        </Layout>
+       )
 };
