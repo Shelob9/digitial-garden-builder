bc9dbc474ddcfe61c4ce7c6a9e54a8110e3774b6
Josh Pollock <josh@calderawp.com>
2020-12-12 01:10:59 +0000

    some type of landing page

--- server/pages/index.tsx
+++ server/pages/index.tsx
@@ -5,7 +5,23 @@ export default function Index({redirect}) {
         Digital Garden Builder Server
          
       </h1>
-      {redirect ? <a href={redirect}>Complete Login</a>: <p>If you tried to login and end up here unclear what to do next, sorry. The URL to redirect back should be in state query param, it's not though...</p>}
+    {redirect && <a href={redirect}>Complete Login</a>}
+    <section>
+        <h2>Build Your Garden</h2>
+        <p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSceXRwG_NQ-5sy1lcP613uS_BH2H1JqhuGOzOWbai7XClK3Cw/viewform">
+                Sign Up For Early Access
+            </a>
+        </p>
+    </section>
+    <section>
+        <h2>Learn More</h2>
+        <p>
+            <a href="https://docs.digitalgardenbuilder.app/notes/digital-garden-builder">
+                Documentation
+            </a>
+        </p>
+    </section>
     </>
     
   )
