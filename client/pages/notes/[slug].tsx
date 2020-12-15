842a68f347c0ff8b16b95ac75a856a9c6b9cf1be
Josh Pollock <jpollock412@gmail.com>
2020-12-15 12:01:00 -0500

    rewrite notes slug to generate props. Importing from server broke things

--- client/pages/notes/[slug].tsx
+++ client/pages/notes/[slug].tsx
@@ -8,8 +8,7 @@ import { INote } from "../../../types";
 import { NextSeo } from 'next-seo';
 import { useRouter } from "next/router";
 import { useMemo } from "react";
-
-
+import { noteApiServicefactory } from '../../../server/services/serviceFactories'
 
 const NoteSeo: FC<{ note: INote }> = ({ note })=> {
   let description = note.content ? note.content.substring(0, 240) : '';
@@ -29,11 +28,16 @@ const NoteSeo: FC<{ note: INote }> = ({ note })=> {
 }
 
 const Page: FC<
-  { note?:INote }
-> = ({   note }) => {
+  { note?: INote;slug?:string }
+> = ({  note,slug }) => {
   const { isLoggedIn } = useIsLoggedInAuthorized();
   const { query } = useRouter();
-  let noteOne = useMemo(() => query.slug as string ?? 'digital-garden-builder', [query]);
+  let noteOne = useMemo(() => {
+    if (slug) {
+      return slug;
+    }
+    return query.slug as string ?? 'digital-garden-builder'
+  }, [query,slug]);
   let noteTwo = useMemo(() =>query.noteTwo as string ?? undefined, [query]);
   let noteThree = useMemo(() => query.noteThree as string ?? undefined, [query]);
   return (
@@ -55,8 +59,30 @@ const Page: FC<
     )
     
   }
+// This function gets called at build time
+//Tells next.js which notes to generate files for
+export async function getStaticPaths() {
+  let authToken = process.env.GITHUB_API_TOKEN;
+  let notesApi = await noteApiServicefactory(authToken);
+  let noteIndex = notesApi.noteIndex
+  const paths = noteIndex.map(({ slug}) => ({
+    params: { slug },
+  }))
+  return { paths, fallback: false }
+}
 
-  
+// This also gets called at build time
+//Tells next.js which note to generate with
+export async function getStaticProps({ params }) {
+  const {slug} = params
+  let authToken = process.env.GITHUB_API_TOKEN;
+  let notesApi = await noteApiServicefactory(authToken);
+  const note = await notesApi.fetchNote(slug);
+
+  // Pass post data to the page via props
+  return { props: { note,slug } }
+}
+
 
 export default Page;
 
