55ea41fcef45e467a1ea357d0402ddbc7866b3e4
Josh Pollock <jpollock412@gmail.com>
2020-12-18 14:25:52 -0500

    pur editor back

--- client/pages/notes/edit.tsx
+++ client/pages/notes/edit.tsx
@@ -1,28 +1,14 @@
-import NoteEditor from '../../components/NoteEditor';
+import NoteEditor from 'components/NoteEditor';
+import { NoteLayoutProvider } from 'components/useNoteLayout';
 import {useRouter} from 'next/router';
-import Note from 'components/Note';
-import useNoteLayout, { NoteLayoutProvider } from 'components/useNoteLayout';
-import useIsLoggedInAuthorized from 'hooks/useIsLoggedAuthorized';
 
-const EditorWithNotes = ({slug}) => {
-  const { isNoteOpen, toggleBox } = useNoteLayout();
-  const { isLoggedIn } = useIsLoggedInAuthorized();
-  return (
-    <div className={'note-columns-scrolling-container'}>
-    <div className={'note-columns-container'}>
-        <NoteEditor slug={slug} />
-        <Note slug={'usage'} isOpen={isNoteOpen('one')} position={'one'} toggleBox={() => toggleBox('one')} isLoggedIn={isLoggedIn} />
-      </div>
-    </div>
-  )
-}
 const Page = () => {
   const { query } = useRouter();
   let slug = query && query.note ? query.note as string : '';
   
   return (
     <NoteLayoutProvider noteSlug={slug}>
-     
+      <NoteEditor slug={slug} />
     </NoteLayoutProvider>
    
   )
