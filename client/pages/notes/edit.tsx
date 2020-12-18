8c898f08ee713a2524f625100ffb83ceabaee11f
Josh Pollock <jpollock412@gmail.com>
2020-12-18 14:24:20 -0500

    Revert "show usage when possible"
    This reverts commit cab685f742a8d3d4e494c6647ef86937db34cb2a.

--- client/pages/notes/edit.tsx
+++ client/pages/notes/edit.tsx
@@ -3,25 +3,15 @@ import {useRouter} from 'next/router';
 import Note from 'components/Note';
 import useNoteLayout, { NoteLayoutProvider } from 'components/useNoteLayout';
 import useIsLoggedInAuthorized from 'hooks/useIsLoggedAuthorized';
-import useNotes from 'components/useNotes';
-import { useMemo } from 'react';
 
 const EditorWithNotes = ({slug}) => {
   const { isNoteOpen, toggleBox } = useNoteLayout();
   const { isLoggedIn } = useIsLoggedInAuthorized();
-  const { getNote } = useNotes();
-  let noteTwoSlug = useMemo(() => {
-    let note = getNote('usage');
-    if (note) {
-      return note.slug;
-    }
-  }, []);
   return (
     <div className={'note-columns-scrolling-container'}>
     <div className={'note-columns-container'}>
         <NoteEditor slug={slug} />
-        {noteTwoSlug && <Note slug={noteTwoSlug} isOpen={isNoteOpen('one')} position={'one'} toggleBox={() => toggleBox('one')} isLoggedIn={isLoggedIn} />
-        }
+        <Note slug={'usage'} isOpen={isNoteOpen('one')} position={'one'} toggleBox={() => toggleBox('one')} isLoggedIn={isLoggedIn} />
       </div>
     </div>
   )
@@ -32,7 +22,7 @@ const Page = () => {
   
   return (
     <NoteLayoutProvider noteSlug={slug}>
-      <EditorWithNotes slug={slug} />
+     
     </NoteLayoutProvider>
    
   )
