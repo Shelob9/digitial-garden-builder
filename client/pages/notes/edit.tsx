cab685f742a8d3d4e494c6647ef86937db34cb2a
Josh Pollock <jpollock412@gmail.com>
2020-12-18 14:13:12 -0500

    show usage when possible

--- client/pages/notes/edit.tsx
+++ client/pages/notes/edit.tsx
@@ -3,15 +3,25 @@ import {useRouter} from 'next/router';
 import Note from 'components/Note';
 import useNoteLayout, { NoteLayoutProvider } from 'components/useNoteLayout';
 import useIsLoggedInAuthorized from 'hooks/useIsLoggedAuthorized';
+import useNotes from 'components/useNotes';
+import { useMemo } from 'react';
 
 const EditorWithNotes = ({slug}) => {
   const { isNoteOpen, toggleBox } = useNoteLayout();
   const { isLoggedIn } = useIsLoggedInAuthorized();
+  const { getNote } = useNotes();
+  let noteTwoSlug = useMemo(() => {
+    let note = getNote('usage');
+    if (note) {
+      return note.slug;
+    }
+  }, []);
   return (
     <div className={'note-columns-scrolling-container'}>
     <div className={'note-columns-container'}>
         <NoteEditor slug={slug} />
-        <Note slug={'usage'} isOpen={isNoteOpen('one')} position={'one'} toggleBox={() => toggleBox('one')} isLoggedIn={isLoggedIn} />
+        {noteTwoSlug && <Note slug={noteTwoSlug} isOpen={isNoteOpen('one')} position={'one'} toggleBox={() => toggleBox('one')} isLoggedIn={isLoggedIn} />
+        }
       </div>
     </div>
   )
@@ -22,7 +32,7 @@ const Page = () => {
   
   return (
     <NoteLayoutProvider noteSlug={slug}>
-     
+      <EditorWithNotes slug={slug} />
     </NoteLayoutProvider>
    
   )
