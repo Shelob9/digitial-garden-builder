05b0c927e4702e4a14d2ac9244281a3cd4c244a7
Josh Pollock <jpollock412@gmail.com>
2020-12-29 18:03:36 -0500

    save and get links

--- server/services/GraphDb.ts
+++ server/services/GraphDb.ts
@@ -45,11 +45,12 @@ class Links extends JsonBased<GRAPH_NODE, GRAPH_NODES> {
 		await this.maybeFetchIndex()
 		let node = this.jsonService.entityFactory(data)
 		this.index.push(node)
-		this.jsonService.client.saveFile(
+		await this.jsonService.client.saveFile(
 			JSON.stringify(this.index),
 			this.indexPath,
 			`Added node ${node.id}`
 		)
+		return node
 	}
 
 	findLink = async (id: string): Promise<GRAPH_NODE | undefined> => {
@@ -70,37 +71,73 @@ class Links extends JsonBased<GRAPH_NODE, GRAPH_NODES> {
 	}
 }
 
+/**
+ * Function that is supplied generic Entity and returns linked entities.
+ *
+ * Return is a GRAPH_NODES
+ * 	Each GRAPH_NODE.from value should match Entity.id. GRAPH_NODES.to are the linked entities.
+ *
+ */
+export type entityLinksFinder<Entity> = (entity: Entity) => GRAPH_NODES
+
 class GraphDb<Entity, Collection> {
 	linksDb: Links
 	dataDb: JsonService<Entity, Collection>
-	constructor(dataDb: JsonService<Entity, Collection>, linksDb: Links) {
+	entityLinksFinder: entityLinksFinder<Entity>
+	constructor(
+		dataDb: JsonService<Entity, Collection>,
+		linksDb: Links,
+		entityLinksFinder: entityLinksFinder<Entity>
+	) {
 		this.dataDb = dataDb
 		this.linksDb = linksDb
+		this.entityLinksFinder = entityLinksFinder
 	}
 
-	getItem = async (id: string) => {
+	getItem = async (
+		id: string
+	): Promise<{ item: Entity; linkedItems: GRAPH_NODES }> => {
 		let item = await this.dataDb.getItem(id)
-		//get links?
-		return item
+		let linkedItems = await this.getLinks(this.entityLinksFinder(item))
+		return { item, linkedItems }
 	}
 
-	addItem = async (item: Entity) => {
+	getLinks = async (linkedItems: GRAPH_NODES): Promise<GRAPH_NODES> => {
+		let _linkedItems: GRAPH_NODES = []
+		linkedItems.forEach(async (node) => {
+			let link = await this.linksDb.findLink(node.id)
+			if (!link) {
+				link = await this.linksDb.addLink(node)
+			}
+			return link
+		})
+		return _linkedItems
+	}
+
+	addItem = async (
+		item: Entity
+	): Promise<{ item: Entity; linkedItems: GRAPH_NODES }> => {
 		item = await this.dataDb.saveItem(
 			//@ts-ignore
 			item.id,
 			item
 		)
-		//add links
-		return item
+		let linkedItems = this.entityLinksFinder(item)
+		if (linkedItems.length) {
+			linkedItems = await this.getLinks(linkedItems)
+		}
+
+		return { item, linkedItems }
 	}
 }
 
 export function graphDbFactory<Entity, Collection>(
 	args: jsonServiceFactoryArgs,
-	jsonDb: JsonService<Entity, Collection>
+	jsonDb: JsonService<Entity, Collection>,
+	entityLinksFinder: entityLinksFinder<Entity>
 ): GraphDb<Entity, Collection> {
 	let linksDb = graphDbLinksFactory(args)
-	return new GraphDb<Entity, Collection>(jsonDb, linksDb)
+	return new GraphDb<Entity, Collection>(jsonDb, linksDb, entityLinksFinder)
 }
 
 export function graphDbLinksFactory(args: jsonServiceFactoryArgs) {
