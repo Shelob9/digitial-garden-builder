d5058f5e586a4212687948e3f01979d8c504477b
Josh Pollock <jpollock412@gmail.com>
2020-12-30 13:36:23 -0500

    fake buttons

--- client/pages/settings/index.tsx
+++ client/pages/settings/index.tsx
@@ -3,10 +3,7 @@ import { FC, forwardRef, useRef, useState } from 'react';
 import Layout from '../../components/Layout';
 import { GardenConfig } from '../../../types/config';
 import useNotes, {  NotesProvider, useNoteSettings } from '../../components/useNotes';
-import useUserToken from 'hooks/useUserCookie';
-import { useMemo } from 'react';
-import useGardenServer from 'hooks/useGardenServer';
-import { useEffect } from 'react';
+
 
 const FieldWrapper = ({ children }) => {
     return (
@@ -60,6 +57,67 @@ const NoteSelector = ({selectedNote,setSelectedNote}) => {
         </FieldWrapper>
     )
 }
+
+
+const FakeButton: FC<{
+    name: 'wordpress' | 'roam' | 'medium'|'twitter',
+    children: string,
+    open: 'wordpress' | 'roam' | 'medium'|'twitter' |false,
+    onClick: (name:'wordpress' | 'roam' | 'medium'|'twitter' ) => void
+}> = ({ children, name, open,onClick }) => (
+    <FieldWrapper>
+        {name === open ? (
+            <span>Feature Comming Soon.</span>
+        ) :
+            <button onClick={() => onClick(name)}>{children}</button>
+        }
+    </FieldWrapper>
+)
+const ConnectionSettings = () => {
+    const [openButton, setOpenButton] = useState<'wordpress' | 'roam' | 'medium' |'twitter' |false>(false);
+    return(
+        <>
+            <h3>Publish</h3>
+            <>
+                <FakeButton
+                    name={'wordpress'}
+                    open={openButton}
+                    onClick={() => setOpenButton('wordpress')}
+                >
+                    Connect To WordPress
+                </FakeButton>
+            </>
+            <>
+                <FakeButton
+                    name={'medium'}
+                    open={openButton}
+                    onClick={() => setOpenButton('medium')}
+                >
+                    Connect To Medium
+                </FakeButton>
+            </>
+            <h3>Add Your Other Parts</h3>
+            <>
+                <FakeButton
+                    name={'roam'}
+                    open={openButton}
+                    onClick={() => setOpenButton('roam')}
+                >
+                    Connect To Roam
+                </FakeButton>
+            </>
+            <>
+                <FakeButton
+                    name={'twitter'}
+                    open={openButton}
+                    onClick={() => setOpenButton('twitter')}
+                >
+                    Connect To Twitter
+                </FakeButton>
+            </>
+       </>     
+    )
+}
 const Settings = () => {
 
     const { settings,saveSettings } = useNoteSettings();
@@ -87,62 +145,79 @@ const Settings = () => {
             () => setIsSaving(false)
         );
     }
+
+    const GardenSettings = () => (
+        <>
+           <section>
+                <TextField
+                    disabled={isSaving}
+                    ref={siteNameRef}
+                    label={'Site Name'}
+                    defaultValue={settings.siteName}
+                />
+                <TextField
+                    disabled={isSaving}
+                    ref={siteTwitterRef}
+                    label={'Site Twitter'}
+                    defaultValue={settings.siteTwitter}
+                />
+            </section>
+            <section>
+                <TextField
+                    disabled={isSaving}
+                    ref={authorNameRef}
+                    label={'Author Name'}
+                    defaultValue={settings.authorName}
+                />
+                <TextField
+                    disabled={isSaving}
+                    ref={authorTwitterRef}
+                    label={'Author Twitter'}
+                    defaultValue={settings.authorTwitter}
+                />
+            </section> 
+            <section>
+                <NoteSelector
+                    selectedNote={defaultNote}
+                    setSelectedNote={setDefaultNote}
+                />
+            </section>
+            <section>
+                <button
+                    disabled={isSaving}
+                    onClick={(e) => {
+                    e.preventDefault();
+                    onSave();
+                }}>
+                    {!isSaving ? 'Save' : 'Saving'}
+                </button>
+            </section>   
+        </>
+    )
     return (
         <>
             <Layout pageDisplayTitle={'Settings'}>
                 <>
-                    {settings ? <form id={"settings"} onSubmit={e => {
-                    e.preventDefault();
-                    onSave();
-                }}
-                >
-                    <section>
-                            <TextField
-                                disabled={isSaving}
-                                ref={siteNameRef}
-                                label={'Site Name'}
-                                defaultValue={settings.siteName}
-                            />
-                            <TextField
-                                disabled={isSaving}
-                                ref={siteTwitterRef}
-                                label={'Site Twitter'}
-                                defaultValue={settings.siteTwitter}
-                            />
-                    </section>
-                    <section>
-                            <TextField
-                                disabled={isSaving}
-                                ref={authorNameRef}
-                                label={'Author Name'}
-                                defaultValue={settings.authorName}
-                            />
-                            <TextField
-                                disabled={isSaving}
-                                ref={authorTwitterRef}
-                                label={'Author Twitter'}
-                                defaultValue={settings.authorTwitter}
-                            />
-                        </section> 
-                    <section>
-                        <NoteSelector
-                            selectedNote={defaultNote}
-                            setSelectedNote={setDefaultNote} />
-                        </section>
-                        <section>
-                            <button
-                                disabled={isSaving}
-                                onClick={(e) => {
+                    {settings ?
+                        <form id={"settings"} onSubmit={e => {
                                 e.preventDefault();
                                 onSave();
-                            }}>
-                                {!isSaving ? 'Save' : 'Saving'}
-                            </button>
-                        </section>          
-                    </form> :<div>Loading</div>}
-                    </>
-                
-                </Layout>
+                            }}
+                        >
+                            <div className={'grid grid-cols-3 gap-4'}>
+                                <div>
+                                    <h2>Garden Settings</h2>
+                                    <GardenSettings /> 
+                                </div>
+                                <div>
+                                    <h2>Connections</h2>
+                                    <ConnectionSettings />
+                                </div>
+                            </div> 
+                        </form>
+                        : <div><span className="animate-pulse">Loading</span><span className="animate-ping">...</span></div>}
+                </>
+            </Layout>
             
         </>
         
