34773c1e2956574312b9dd5e9656116cd6a6caa1
Josh Pollock <jpollock412@gmail.com>
2020-12-31 12:51:25 -0500

    button toolbar

--- client/components/Note.tsx
+++ client/components/Note.tsx
@@ -62,6 +62,7 @@ const NoteTitle: React.FC<{ note: INote,className?:string }> = ({ note,className
 	<h1 className={className}>{note.title}</h1>
 )
 
+
 //Display one note
 const Note: FC<{
 	note?: INote;
@@ -79,7 +80,7 @@ const Note: FC<{
 	const { focusNote, setFocusNote } = useNoteLayout();
 	const outterClassName = useMemo(
 		() => `note-container ${isOpen ? 'note-open' : 'note-closed'} ${focusNote === position ? 'note-focus' : ''}`,
-		[focusNote, position]
+		[focusNote, position,isOpen]
 	);
 	if (!note) {
 		return (
@@ -110,34 +111,76 @@ const Note: FC<{
 		)
 	}
 
+	const Toolbar = ({ children }) => (
+		<div
+			role={'toolbar'}
+			aria-label={`Controls for note ${note.title}`}
+			aria-controls={`note-${note.slug}`}
+			className={'border border-gray-500 space-y-0 text-center flex justify-center rounded-lg text-lg mb-4'}
+				
+		>
+			{children}
+		</div>
+	);
+	if (!isOpen) {
+		return (<>
+			<div
+				className={outterClassName}
+			>
+				<Toolbar>
+					<a
+						role={'button'}
+						className="bg-white text-black hover:text-white hover:bg-gray-500  border-green-500 border px-4 py-2 mx-0 outline-none focus:shadow-outline"
+						title={'Click To Open'}
+						onClick={(e) => {
+							e.preventDefault();
+							toggleBox()
+						}}
+						>
+							Open
+					</a>
+				</Toolbar>
+			</div>
+		</>);
+	}
+
+
 	let { content } = note;
     return (
         <>
 			<div
 				className={outterClassName}
 			>
-				<div className={'note-buttons'}>
-					<button
-						title={isOpen ? 'Collapse note' : 'Expand Note'}
-						onClick={
-						() => toggleBox()
-					}>
-						{isOpen ? '-' : '+'}
-					</button>
-					{isOpen &&
-						 isLoggedIn && <Link
-							href={isLoggedIn ? `/notes/edit?note=${encodeURIComponent(note.slug)}` : '/login'}
-						>
+				<Toolbar>
+					<a
+						role={'button'}
+						className="bg-white text-black hover:text-white hover:bg-gray-500  border-green-500 border border-r-0 rounded-l-lg px-4 py-2 mx-0 outline-none focus:shadow-outline"
+						title={`Click to ${isOpen ? 'Close' : 'Open'}`}
+						onClick={(e) => {
+							e.preventDefault();
+							toggleBox()
+						}}
+					>
+						{isOpen ? 'Close' : 'Open'}
+					</a>
+					{!isLoggedIn && <Link href={`/notes/edit?note=${note.slug}`}>
 							<a
-								className={'edit-note'}
+								className="bg-white text-black hover:text-white hover:bg-gray-500  border-green-500 border px-4 py-2 mx-0 outline-none focus:shadow-outline"
+								title={'Click To Edit'}
 							>
-								{'Edit'}
+									Edit
 							</a>
-						</Link>	
-					}
-				</div>
+						</Link>}
+					<a
+						className="bg-white text-black hover:text-white hover:bg-gray-500  border-green-500 border rounded-r-lg px-4 py-2 mx-0 outline-none focus:shadow-outline"
+						title={'Click To Edit'}
+					>
+						Share
+					</a>
+				</Toolbar>
                 {isOpen &&
 					<div
+						id={`note-${note.slug}`}
 						className={"note-content"}
 						onClick={() => setFocusNote(position)}
 				>
