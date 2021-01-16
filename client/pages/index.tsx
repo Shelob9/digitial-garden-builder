76e831b69a91ec1804f5f00c3691b85449e30106
Josh Pollock <jpollock412@gmail.com>
2021-01-16 16:50:50 -0500

    pass default note to NoteApp in index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,26 @@
+import { gardenServiceFactory } from 'services/factory';
 import NoteApp from '../components/NoteApp';
 import { NoteLayoutProvider } from '../components/useNoteLayout';
-import { NotesProvider } from '../components/useNotes';
+import { useNoteSettings, useSingleNote } from '../components/useNotes';
 import useIsLoggedIn from '../hooks/useIsLoggedAuthorized'
 
 
 const Index = () => {
-  const { isLoggedIn, } = useIsLoggedIn()
+  const { isLoggedIn, } = useIsLoggedIn();
+  const { defaultNote } = useNoteSettings();
+  const { note } = useSingleNote(defaultNote);
   return (
     <>
       <>
         <NoteLayoutProvider>
-          <NoteApp isLoggedIn={isLoggedIn} />
+          <NoteApp
+            isLoggedIn={isLoggedIn}
+            noteOneSlug={defaultNote} note={note}
+          />
         </NoteLayoutProvider>
       </>
     </>
   )
 }
-
 
 export default Index;
