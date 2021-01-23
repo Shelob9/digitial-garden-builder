e1a19336a976d7cbe457130c8240acbd601d2ae6
Josh Pollock <jpollock412@gmail.com>
2021-01-23 12:15:10 -0500

    extract keywords and prhasese with nlp.

---
+++ server/lib/processKeywords.ts
@@ -0,0 +1,41 @@
+const retext = require('retext')
+const pos = require('retext-pos')
+const keywords = require('retext-keywords')
+const toString = require('nlcst-to-string')
+
+function stringify(value) {
+	return toString(value)
+}
+
+const processKeywords = async (text: string) => {
+	return new Promise(async (resolve, reject) => {
+		retext()
+			.use(pos)
+			.use(keywords)
+			.process(text, (err, file) => {
+				if (err) {
+					reject(err)
+				}
+				let data = {
+					keywords: [],
+					phrases: [],
+				}
+
+				file.data.keywords.forEach(function (keyword) {
+					data.keywords.push(toString(keyword.matches[0].node))
+				})
+
+				file.data.keyphrases.forEach(function (phrase) {
+					phrase.matches[0].nodes.forEach((value) => {
+						value = stringify(value)
+						if (value && value.length) {
+							data.phrases.push(value)
+						}
+					})
+				})
+				resolve(data)
+			})
+	})
+}
+
+export default processKeywords
