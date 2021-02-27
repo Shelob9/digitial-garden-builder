7e475ff899a8e0405aca48afebc57f19515ea7cf
Josh Pollock <jpollock412@gmail.com>
2021-02-27 13:31:02 -0500

    remove more code

--- server/lib/getOctoKit.ts
+++
@@ -1,7 +0,0 @@
-import { Octokit } from '@octokit/rest'
-
-export function getOctokit(authToken?: string): Octokit {
-	return new Octokit({
-		auth: authToken ?? process.env.GITHUB_API_TOKEN,
-	})
-}
